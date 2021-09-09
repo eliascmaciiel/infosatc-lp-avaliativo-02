d06595f723cd0cb438b3ee51b184818854b63c10
eliascmaciiel <eliascmaciiel@gmail.com>
2021-09-09 16:37:06 -0300

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,33 @@
+lista_pc = []
+
+lista_pc.append ("Processador Intel Core i9 11900K 11ª geração")
+
+lista_pc.append ("Placa de vídeo NVIDIA GeForce RTX 2080TI 11 GB")
+
+lista_pc.append ("Memória 2x 32GB DDR4 3000Mhz")
+
+lista_pc.append ("HD 4TB Seagate 7200 RPM")
+
+lista_pc.append ("SSD 2TB NVME SN850 Leitura 7000MB/s - Gravação 5300MB/s")
+
+lista_pc.append ("Cooler Watercooler ML240L V2 RGB")
+
+lista_pc.append ("Placa Mãe Asus Z590-P")
+
+lista_pc.append ("Fonte Cooler Master 800W Gold")
+
+lista_pc.append ("Gabinete Cooler Master COSMOS C700M")
+
+print("Lista PC: {}".format(lista_pc))
+
+del lista_pc[1]
+del lista_pc[7]
+
+print("")
+
+print("Lista PC (sem placa de vídeo e sem gabinete): {}".format(lista_pc))
+
+
+
+
+
