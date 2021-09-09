576a4d1d5ead5699366e0ce16fb6945681513d0f
eliascmaciiel <eliascmaciiel@gmail.com>
2021-09-09 15:56:55 -0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,14 @@
+lista = []
+lista.append("Thiago")
+lista.append("Gabriel")
+lista.append("Elias")
+lista.append("Augusto")
+lista.append("Giordano")
+lista.append("Lucas")
+lista.append("Nicolas")
+
+lista_nova = lista.copy()
+
+print ("Lista antiga: ", lista)
+
+print ("Nova lista: ", lista_nova)
